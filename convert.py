108693a29a66476105ba65f6f1d3f603e4ff99c8
GeorgeCeladon <64736051+GeorgeCeladon@users.noreply.github.com>
2022-07-09 08:32:38 +0200

    W_LEVEL CONTROL!!!

--- convert.py
+++ convert.py
@@ -19,16 +19,24 @@ jpg_folder_path = "D:\#Medicina\#Radiodiagnostica\[#] Archivio Referti\[=] PACS\
 images_path = os.listdir(folder_path)
 for n, image in enumerate(images_path):
     ds = dicom.dcmread(os.path.join(folder_path, image))
+
+    ds.WindowCenter = "4000"
+    ds.WindowWidth = "6000"
+
     pixel_array_numpy = ds.pixel_array
     pixel_array_numpy_2 = apply_voi_lut(
-        pixel_array_numpy, ds, index=1, prefer_lut=True)
+        pixel_array_numpy, ds, prefer_lut=False)
+    pixel_array_numpy_3 = apply_windowing(
+        pixel_array_numpy_2, ds
+    )
+    print(ds)
 
     if PNG == False:
         image = image.replace('.DCM', '.jpg')
     else:
         image = image.replace('.DCM', '.png')
 
-    cv2.imwrite(os.path.join(jpg_folder_path, image), pixel_array_numpy_2)
+    cv2.imwrite(os.path.join(jpg_folder_path, image), pixel_array_numpy_3)
 
     if n % 50 == 0:
         print('{} image converted'.format(n))
